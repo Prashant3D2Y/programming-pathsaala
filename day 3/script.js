6c4977bd51e77979f0366fef3ab3d26bc935f31e
unknown <prashantsri1.p@gmail.com>
2024-04-04 17:19:59 +0530

    day-3

---
+++ day 3/script.js
@@ -0,0 +1,214 @@
+
+// var meassage = "hello world";
+// console.log( meassage)
+
+
+// var meassage = "hello world";
+
+// function env(){
+// console.log( meassage)
+// var meassage = "hello world";
+//  console.log( meassage)
+//  meassage = "else" ;
+//  console.log( meassage)
+
+// }
+
+// function env(){
+//     for(var i=0; i<5; i++) {
+//         console.log(i)
+//     }
+// }
+
+// env();
+
+
+// var meassage = "hello world";
+
+//   meassage = "else";
+
+// console.log(meassage);
+
+
+// const meassage = "hello world";
+
+
+// console.log(meassage);
+
+
+// let age= 20;
+// console.log(typeof age);
+
+
+// let userAge= `abch`;
+// age= 'abxh';
+// console.log(typeof(userAge))
+
+// console.log(`vafvrgvfvrf ${userAge}`)
+
+
+// let arr = ["a", "b", "c", "d", "e", ];
+
+// console.log(arr);
+
+
+// console.log(1+"i");
+
+// console.log("1"+1);
+
+// console.log("1"+"1");
+// console.log(1+1);
+
+// console.log("1"+"1"+1);
+
+// console.log("1"+1+1);
+
+// console.log("1"+"1"+1)    ;
+// console.log(1+1+"1"+"1");
+
+// let str= '12';
+// let ans = 20;
+
+// let sum = str *1-20;
+// console.log(sum);
+
+
+
+// let str  = '12a';
+// let p = parseInt(str);
+// let n = Number(str);
+// console.log(p,n);
+
+
+// let aa= ['a', 'b', 'c', 'd', 'e', ];
+
+// let cpy = [...aa];
+
+
+
+//  if (aa==cpy){
+// console.log('yes');
+//  }
+//  else{
+// console.log('no');
+//  }
+// if(aa===cpy){
+//     console.log('yes');
+
+// }
+//  else{
+//     console.log('no');
+// }
+
+
+// const str ='khan';
+
+// const userName = `My Name  is `;
+
+
+
+// const ans=`${userName} ${str}`;
+
+// console.log(ans);
+
+// const a = 'my';
+
+// const b= 'name';
+
+// const c= 'is';
+
+// const d= ':';
+
+// const e= 'khan';
+// const ans= `${a} ${b} ${c} ${d} ${str}`;
+// const ans2 = a + '' +b + '' +c+d + '';
+// console.log(ans);
+// console.log(ans2);
+
+
+// function print(str){
+//     console.log(str);
+// }
+// print('hello');
+
+
+
+// function sum(a, b){
+//     if(a&&b)
+
+//     console.log(a+b);
+//     else if(a){
+//         console.log(a);
+//     } 
+//     else { 
+//         console.log(0);
+//     }
+
+// }
+
+// sum();
+
+// let a =20;
+
+// switch(a){
+//     case 1:{
+//         console.log('one');
+//         break;
+//     }
+//     case 2:{
+//         console.log('two');
+//         break;
+//     }
+//     case 3:{
+//         console.log('three');
+//         break;
+//     }
+//     default:{
+//         console.log('default');
+//         break;
+//     }
+// }
+// console.log(null==0);
+
+// console.log(null===0);
+
+// console.log(null==undefined);
+
+// console.log(null===undefined);
+
+// console.log(null==null);
+
+// console.log(null===null);
+
+// console.log(null==NaN);
+
+
+// const obj = new Object();
+// obj[1] = "one";
+
+// obj[2] = "two";
+
+// obj[3] = "three";
+
+// obj[4] = "four";
+// obj ['name']= "prashant";
+
+
+// console.log(obj);
+
+// const obj = {
+//     1: "one",
+//     "name": "prashant",
+//     12 : "twelve"
+// };
+// console.log(obj);
+
+const obj ={
+    'first name': 'prashant',
+    'last name': 'khan',
+    'age': 20,
+    'city': 'bangalore'
+};
+
+const x = prompt ( 'pleaase tell the property to known');
+console.log(obj[x]);
