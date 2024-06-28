b6f65fc96bd5f2681d0824f894f3ec37378ff9a9
Prashant Srivastava <138637829+Prashant3D2Y@users.noreply.github.com>
2024-06-28 15:12:47 +0530

    imageController.js

--- day 13/KIET-MAJOR-BACKEND/controllers/imageController.js
+++ day 13/KIET-MAJOR-BACKEND/controllers/imageController.js
@@ -8,13 +8,14 @@ const generateImage = async (req, res) => {
     let imageUrl = "";
     try{
         const res = await fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
+  fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
   "headers": {
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,hi;q=0.8",
-    "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTgxMjA5NjEsImV4cCI6MTcxODEyNDU2MX0.YTNZreMH1KZ2x4m2Ei-XCc-T4YtfmaGF1zrIDL22G_Q",
+    "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTk1NjczNzMsImV4cCI6MTcxOTU3NDU3M30.KO2gZ5hN3ZXXiEEZ8MDXnz7LdK2XFWVmTVeJo55iCMs",
     "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
-    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryaMjjh4nsT3bYKuuY",
-    "sec-ch-ua": "\"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryb7twkbYXEduqQiXp",
+    "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\"",
     "sec-fetch-dest": "empty",
@@ -23,11 +24,12 @@ const generateImage = async (req, res) => {
   },
   "referrer": "https://hotpot.ai/",
   "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": `------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n77\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nConcept Art 3\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-9T2ArfIrRELBDI7\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-9T2ArfIrRELBDI7.png\r\n------WebKitFormBoundaryaMjjh4nsT3bYKuuY--\r\n`,
+  "body": "------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-hKkKNrEZS16bmff\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-hKkKNrEZS16bmff.png\r\n------WebKitFormBoundaryb7twkbYXEduqQiXp--\r\n",
   "method": "POST",
   "mode": "cors",
   "credentials": "include"
 });
+        // n${searchText}\
         imageUrl  = await res.json();   
         let imageUrlForMongodb = await downloadAndUploadImage(imageUrl,searchText);
         imageCloudURL = imageUrlForMongodb;
